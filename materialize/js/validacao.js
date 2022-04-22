5acc513b7e8474343c49162a8109e35a0383c913
Bruno Cesar <bruno_cco@hotmail.com>
2022-04-22 14:27:04 -0300

    validação
    validação de itens do formulário sem radio e sem checkbox

--- materialize/js/validacao.js
+++ materialize/js/validacao.js
@@ -1,37 +1,40 @@
 let nome = document.getElementById("nome");
+let snome = document.getElementById("snome");
+let idade = document.getElementById("idade");
 let email = document.getElementById("email");
-let celular = document.getElementById("celular");
-let data = document.getElementById("data");
-let sv = document.getElementById("sv");
-let lk = document.getElementById("lk");
-let git = document.getElementById("git");
-let ps = document.getElementById("ps");
+let sexo = document.getElementById("sexo");
+let pais = document.getElementById("pais");
+let lingp = document.getElementById("lingp");
+let areaatuacao = document.getElementById("areaatuacao");
+
 
 function validar(){
     if(nome.value == ''){
         alert("Campo Nome em branco");
     }
+    else if(snome.value == ''){
+        alert("Campo Sobrenome em branco");
+    }
+    else if(idade.value == ''){
+        alert("Campo Idade em branco");
+    }
     else if(email.value == ''){
         alert("Campo E-mail em branco");
     }
-    else if(celular.value == ''){
-        alert("Campo Celular em branco");
+
+    else if(sexo.value == ''){
+        alert("Campo Sexo em branco");
     }
-    else if(data.value == ''){
-        alert("Campo Data em branco");
+    else if(pais.value == ''){
+        alert("Campo Pais em branco");
     }
-    else if(sv.value == ''){
-        alert("Campo Sobre você em branco");
+    else if(lingp.value == ''){
+        alert("Campo Linguagens Preferenciais em branco");
     }
-    else if(lk.value == ''){
-        alert("Campo Linkedin em branco");
+    else if(areaatuacao.value == ''){
+        alert("Campo Áreas de Atuação em branco");
     }
-    else if(git.value == ''){
-        alert("Campo Github em branco");
-    }
-    else if(ps.value == ''){
-        alert("Campo Pretenção Salarial em branco");
-    }else{
+    else{
         alert("Enviado com sucesso!!");
     }
 }
