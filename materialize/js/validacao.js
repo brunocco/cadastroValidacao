af98167c7b784141671aa66963830ca1ee38e940
Bruno Cesar <bruno_cco@hotmail.com>
2022-04-24 17:28:14 -0300

    validação radio button e checkbox
    validação de checkbox e radio button

--- materialize/js/validacao.js
+++ materialize/js/validacao.js
@@ -5,9 +5,15 @@ let email = document.getElementById("email");
 let sexo = document.getElementById("sexo");
 let pais = document.getElementById("pais");
 let lingp = document.getElementById("lingp");
-let areaatuacao = document.getElementById("areaatuacao");
+let areaatuacao1 = document.getElementById("areaatuacao1");
+let areaatuacao2 = document.getElementById("areaatuacao2");
+let areaatuacao3 = document.getElementById("areaatuacao3");
+let arquivoInput = document.getElementById("arquivo");
 
 function validar(){
+
+
+
     if(nome.value == ''){
         alert("Campo Nome em branco");
     }
@@ -21,14 +27,13 @@ function validar(){
         alert("Campo E-mail em branco");
     }
 
-    else if(frmreset.sexo[0].checked == '' || frmreset.sexo[0].checked == null  ){
+    else if(frmreset.sexo[0].checked == '' && frmreset.sexo[1].checked == ''){
         alert("Campo Sexo em branco");
     }
 
-    else if(frmreset.areaatuacao[0].checked == '' || frmreset.areaatuacao[0].checked == null  ){
-        alert("Campo Área de atuação em branco");
+    else if(arquivoInput.value == ''){
+        alert("Campo imagem em branco");
     }
-
 
     else if(pais.value == ''){
         alert("Campo Pais em branco");
@@ -36,33 +41,44 @@ function validar(){
     else if(lingp.value == ''){
         alert("Campo Linguagens Preferenciais em branco");
     }
-    else if(areaatuacao.value == '' ){
-        alert("Campo Áreas de Atuação em branco");
+
+    else if(frmreset.areaatuacao1.checked == '' && frmreset.areaatuacao2.checked == '' && frmreset.areaatuacao3.checked == '' ){
+        alert("Campo Área de atuação em branco");
     }
+
+
     else{
         alert("Enviado com sucesso!!");
     }
 }
 
-/*
-if(frm.sexo.value == "" || frm.txt_endereco.value == null ||
-frm.txt_endereco.value.length < 3) {
- // Exibiremos um alerta, caso o campo esteja vazio.
- alert("Por favor, informe o seu endereço.");
 
 
- escolhaSexo = -1;
- for (x = frm.sexo.length -1; x > -1; x--) {
 
-    if(frm.rdo_sexo[x].checked) {
-        escolhaSexo = x;
+/* -validar tamanho, tipo e prevew de arquivo-
+
+function validarArquivo(){
+    var arquivoInput = document.getElementById('arquivo');
+    var caminhoArquivo = arquivoInput.value;
+    var extensoesPermitidas = /(.jpg|.jpeg|.png)$/i;
+    if(!extensoesPermitidas.exec(caminhoArquivo)){
+        alert('Por favor envie um arquivo que tenha as extensões.jpeg/.jpg/.png');
+        arquivoInput.value = '';
+        return false;
+    }else{
+        if (arquivoInput.files && arquivoInput.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                document.getElementById('visualizarImagem').innerHTML = '';
+            };
+            reader.readAsDataURL(arquivoInput.files[0]);
+ console.log(arquivoInput.files[0].size / 1024 / 1024);
+ console.log(arquivoInput.files[0].size);
+ if (arquivoInput.files[0].size > 2097152) { 
+                alert("Tamanho do arquivo deve ser 2 MB!");
+                return false;
+            }
+        }
     }
- }
-   if(escolhaSexo == -1) {
-    alert("Qual o seu sexo?");
-    frm.sexo[0].focus();
-    return false;
-   }
 }
-
 */
