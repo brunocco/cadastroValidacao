b95d864991afa4d7a892eea77cf6600d19bc7b55
Bruno Cesar <bruno_cco@hotmail.com>
2022-04-24 18:48:12 -0300

    validação apenas no onsubmit
    retirando o onclick e deixando a validacao pela tag onsubmit

--- materialize/js/validacao.js
+++ materialize/js/validacao.js
@@ -16,40 +16,56 @@ function validar(){
 
     if(nome.value == ''){
         alert("Campo Nome em branco");
+        return false;
     }
     else if(snome.value == ''){
         alert("Campo Sobrenome em branco");
+        return false;
     }
     else if(idade.value == ''){
         alert("Campo Idade em branco");
+        return false;
     }
     else if(email.value == ''){
         alert("Campo E-mail em branco");
+        return false;
     }
 
     else if(frmreset.sexo[0].checked == '' && frmreset.sexo[1].checked == ''){
         alert("Campo Sexo em branco");
+        return false;
     }
+    
 
     else if(arquivoInput.value == ''){
         alert("Campo imagem em branco");
+        return false;
     }
+   
 
     else if(pais.value == ''){
         alert("Campo Pais em branco");
+        return false;
     }
+    
     else if(lingp.value == ''){
         alert("Campo Linguagens Preferenciais em branco");
+        return false;
     }
+    
 
     else if(frmreset.areaatuacao1.checked == '' && frmreset.areaatuacao2.checked == '' && frmreset.areaatuacao3.checked == '' ){
         alert("Campo Área de atuação em branco");
+        return false;
     }
+    
 
 
     else{       
-        alert("Enviado com sucesso!!");      
-    }
+        alert("Enviado com sucesso!!");
+        return true;
+    }      
+    
 }
 
 
