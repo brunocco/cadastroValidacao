66f1cd18ae9d526cb04408695c668192bcc405f7
Bruno Cesar <bruno_cco@hotmail.com>
2022-05-03 09:31:53 -0300

    limpeza codigo
    atualização de codigos

--- materialize/js/validacao.js
+++ materialize/js/validacao.js
@@ -68,33 +68,3 @@ function validar(){
     
 }
 
-
-
-
-/* -validar tamanho, tipo e prevew de arquivo-
-
-function validarArquivo(){
-    var arquivoInput = document.getElementById('arquivo');
-    var caminhoArquivo = arquivoInput.value;
-    var extensoesPermitidas = /(.jpg|.jpeg|.png)$/i;
-    if(!extensoesPermitidas.exec(caminhoArquivo)){
-        alert('Por favor envie um arquivo que tenha as extensões.jpeg/.jpg/.png');
-        arquivoInput.value = '';
-        return false;
-    }else{
-        if (arquivoInput.files && arquivoInput.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                document.getElementById('visualizarImagem').innerHTML = '';
-            };
-            reader.readAsDataURL(arquivoInput.files[0]);
- console.log(arquivoInput.files[0].size / 1024 / 1024);
- console.log(arquivoInput.files[0].size);
- if (arquivoInput.files[0].size > 2097152) { 
-                alert("Tamanho do arquivo deve ser 2 MB!");
-                return false;
-            }
-        }
-    }
-}
-*/
